e5a092703f2ea86aeb3c9779a830367d899e110d
luis.reyes <101216.Mch>
2023-04-28 16:54:19 +0200

    Module 4: commit #2

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -6,17 +6,17 @@ import time
 start_time = None
 total_written = 0
 
-@hooks.event("start_build")
+@hooks.register("start_build")
 def start_build():
     global start_time
     start_time = datetime.datetime.now()
 
-@hooks.event("written")
+@hooks.register("written")
 def written():
     global total_written
     total_written += 1
 
-@hooks.event("stats")
+@hooks.register("stats")
 def stats():
     final_time = datetime.datetime.now() - start_time
     average = final_time / total_written if total_written != 0 else 0
